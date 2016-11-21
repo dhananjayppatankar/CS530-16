b28a02ccf633f00a63a5297ee5352dbe289e47cd
echo49 <liyuanqin@my.ccsu.edu>
2016-11-20 22:23:02 -0500

    filter live bus info not on fastrak

--- CTfastrak_Website__Iteration 2 (Bootstrap, JavaScript)/scripts/Live_vehicle_Location.js
+++ CTfastrak_Website__Iteration 2 (Bootstrap, JavaScript)/scripts/Live_vehicle_Location.js
@@ -1,4 +1,5 @@
 var busArray = [];
+var fastrakRoutes = [8336, 8473, 8337, 8474, 8339, 8476, 8340, 8477, 8341, 8478, 8342, 8479, 8343, 8480, 8344, 8481]; 
 
 function getBusLocations(){
 
@@ -8,7 +9,8 @@ var map;
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "http://65.213.12.244/realtimefeed/vehicle/vehiclepositions.json", true);
 xhr.onload = function () {
-	var feed = eval("("+ xhr.response +")");
+	//var feed = eval("("+ xhr.response +")");
+	var feed = JSON.parse(xhr.responseText);	
 	var coords = "";
 	var i;
   for (var i = 0; i < busArray.length; i++) {
@@ -18,17 +20,24 @@ xhr.onload = function () {
   busArray = [];
 
 	for(i=0; i < Object.keys(feed["entity"]).length;i++){
-		
-		var marker = new google.maps.Marker({
+			
+		var isfastrakroutes = isInArray(feed["entity"][i].vehicle.trip.route_id, fastrakRoutes);		
+		if (isfastrakroutes) {
+			var marker = new google.maps.Marker({
 				position: {lat: feed["entity"][i].vehicle.position.latitude, lng: feed["entity"][i].vehicle.position.longitude},
 				map: _map,
 				title: feed["entity"][i].id,
 				icon: 'Bus_Postion_Icon.png'});
 
-		busArray.push(marker);
+			busArray.push(marker);
+			}
 		}
 }
 xhr.send()
+}
+
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
 }
 
 //Reload realtime data every 9 seconds
