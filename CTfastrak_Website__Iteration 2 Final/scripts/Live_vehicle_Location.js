d1dddb05f09848f756a50d8d93ac37089b5047de
SACHIN PATEL <snpatel@my.ccsu.edu>
2016-11-21 11:49:05 -0500

    remove bus location filter

--- CTfastrak_Website__Iteration 2 Final/scripts/Live_vehicle_Location.js
+++ CTfastrak_Website__Iteration 2 Final/scripts/Live_vehicle_Location.js
@@ -1,16 +1,18 @@
 var busArray = [];
-var fastrakRoutes = [8336, 8473, 8337, 8474, 8339, 8476, 8340, 8477, 8341, 8478, 8342, 8479, 8343, 8480, 8344, 8481]; 
 
 function getBusLocations(){
+
+
+var validBusArray = ['711','712','1432','1434','1435','1437','1438','1441','1443','1444','1447','1448','1449','1450','1454','1455','1457','1458','1463','1464','1465','1467','1471','1472','1473','1475','1476','A73','A77','A82']
 
 var markers = [];
 var directionsDisplay;
 var map;
+
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "http://65.213.12.244/realtimefeed/vehicle/vehiclepositions.json", true);
 xhr.onload = function () {
-	//var feed = eval("("+ xhr.response +")");
-	var feed = JSON.parse(xhr.responseText);	
+	var feed = eval("("+ xhr.response +")");
 	var coords = "";
 	var i;
   for (var i = 0; i < busArray.length; i++) {
@@ -19,29 +21,33 @@ xhr.onload = function () {
   
   busArray = [];
 
+
+
 	for(i=0; i < Object.keys(feed["entity"]).length;i++){
-			
-		var isfastrakroutes = isInArray(feed["entity"][i].vehicle.trip.route_id, fastrakRoutes);		
-		if (isfastrakroutes) {
-			var marker = new google.maps.Marker({
+		//if (validBusArray.indexOf(feed["entity"][i].id) > -1){
+		
+		var marker = new google.maps.Marker({
 				position: {lat: feed["entity"][i].vehicle.position.latitude, lng: feed["entity"][i].vehicle.position.longitude},
 				map: _map,
 				title: feed["entity"][i].id,
 				icon: 'Bus_Postion_Icon.png'});
 
-			busArray.push(marker);
-			}
+		busArray.push(marker);
 		}
+	//}
+
 }
+//_displayDirections.setMap(map);
+
 xhr.send()
 }
 
-function isInArray(value, array) {
-  return array.indexOf(value) > -1;
-}
+//Reload realtime data every 30 seconds
+setInterval(function(){clearTrip();getBusLocations()}, 3000);
+//setInterval(function(){getTrips()}, 30000);
+//setInterval(function(){getAlerts()}, 30000);
 
-//Reload realtime data every 9 seconds
-setInterval(function(){clearTrip();getBusLocations()}, 9000);
+
 
 // Sets the map on all markers in the array.
 function clearTrip() {
